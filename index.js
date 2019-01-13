067d258aa37a2170601f0dc031313441445572ac
Muhammad Rizki FIkriansyah <fikriansyahmr@gmail.com>
2019-01-13 18:42:45 +0100

    fix cors

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ const corsWhitelist = [
 ];
 const corsOptions = {
   origin: (origin, callback) => {
-    if (corsWhitelist.indexOf(origin) !== -1) {
+    console.log(origin);
+    if (corsWhitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
